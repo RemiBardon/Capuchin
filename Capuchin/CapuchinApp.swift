51c9ed8f2f1c7ce681db67ce2fbcee63f25026e2
Rémi Bardon <remi.bardon.dev@gmail.com>
2023-05-14 17:14:55 +0200

    🎉 Initial commit

---
+++ Capuchin/CapuchinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapuchinApp.swift
+//  Capuchin
+//
+//  Created by Rémi Bardon on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CapuchinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
